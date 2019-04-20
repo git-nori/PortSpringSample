6931a9d4096d8e8f5197b147b086513192ca0488
HironoriSato <maza.hs5360@gmail.com>
2019-04-20 11:40:03 +0900

    [update]元レコードをDB上に追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@ VALUES (1, 'nori', 30);
 
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
 VALUES ('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', '28', false, 'ROLE_ADMIN');
+INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
+VALUES ('tanaka@xxx.co.jp', 'password', '田中太郎', '1990-02-02', '30', false, 'ROLE_ADMIN');
