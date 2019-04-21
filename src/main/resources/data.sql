fa18a8e522fd8f4fc26c49fa164b47945ce96491
HironoriSato <maza.hs5360@gmail.com>
2019-04-21 22:29:46 +0900

    [update]テストデータをDBに追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,5 @@ INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
 VALUES ('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', '28', false, 'ROLE_ADMIN');
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
 VALUES ('tanaka@xxx.co.jp', 'password', '田中太郎', '1990-02-02', '30', false, 'ROLE_ADMIN');
+INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
+VALUES ('tamura@xxx.co.jp', 'password', '田村達也', '1986-11-05', '31', false, 'ROLE_GENERAL');
