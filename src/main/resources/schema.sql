886ad834800c594c6e1c9d879077c0bfc9fb459c
HironoriSato <maza.hs5360@gmail.com>
2019-04-22 22:05:23 +0900

    [update]不要なテーブルの削除

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,3 @@
-CREATE TABLE IF NOT EXISTS employee(
-	employee_id INT PRIMARY KEY,
-	employee_name VARCHAR(50),
-	age INT
-);
-
 CREATE TABLE IF NOT EXISTS m_user(
 	user_id VARCHAR(50) PRIMARY KEY,
 	password VARCHAR(100),
@@ -12,4 +6,4 @@ CREATE TABLE IF NOT EXISTS m_user(
 	age INT,
 	marriage BOOLEAN,
 	role VARCHAR(50)
-)
+);
