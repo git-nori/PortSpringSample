df3bb7ce2ed56e93599229612410115fbb703c2a
HironoriSato <maza.hs5360@gmail.com>
2019-04-22 22:03:25 +0900

    [update]DBの既存データのパスワードを暗号化

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
-VALUES ('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', '28', false, 'ROLE_ADMIN');
+VALUES ('yamada@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '山田太郎', '1990-01-01', '28', false, 'ROLE_ADMIN');
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
-VALUES ('tanaka@xxx.co.jp', 'password', '田中太郎', '1990-02-02', '30', false, 'ROLE_ADMIN');
+VALUES ('tanaka@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '田中太郎', '1990-02-02', '30', false, 'ROLE_ADMIN');
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
-VALUES ('tamura@xxx.co.jp', 'password', '田村達也', '1986-11-05', '31', false, 'ROLE_GENERAL');
+VALUES ('tamura@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '田村達也', '1986-11-05', '31', false, 'ROLE_GENERAL');
