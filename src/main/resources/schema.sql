604fdbe8ba16efcb635bb81b9d2c4c7cf9600f36
HironoriSato <maza.hs5360@gmail.com>
2019-04-25 23:38:30 +0900

    [update]テストデータとなる記事のテーブルとレコードを追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,12 @@ CREATE TABLE IF NOT EXISTS m_user(
 	gender BOOLEAN,
 	role VARCHAR(50)
 );
+
+CREATE TABLE IF NOT EXISTS articles(
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  user_id VARCHAR(50) NOT NULL,
+  title VARCHAR(30),
+  content TEXT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES m_user (user_id)
+);
