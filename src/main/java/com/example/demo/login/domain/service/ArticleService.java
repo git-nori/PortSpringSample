9f2e831e7232c771fbc81b8eb1dd0e8b534ae3ac
HironoriSato <maza.hs5360@gmail.com>
2019-04-26 00:51:41 +0900

    [update]ArticleServiceクラスに@Transactionalを追加

--- src/main/java/com/example/demo/login/domain/service/ArticleService.java
+++ src/main/java/com/example/demo/login/domain/service/ArticleService.java
@@ -2,10 +2,9 @@ package com.example.demo.login.domain.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.login.domain.model.Article;
 import com.example.demo.login.domain.repository.ArticleDao;
