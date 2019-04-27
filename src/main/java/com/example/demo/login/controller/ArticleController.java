3fee24f7b3d2acf2639cb09148eb6c9b768761c9
HironoriSato <maza.hs5360@gmail.com>
2019-04-28 00:47:26 +0900

    [update]更新処理をコントローラクラスに追加

--- src/main/java/com/example/demo/login/controller/ArticleController.java
+++ src/main/java/com/example/demo/login/controller/ArticleController.java
@@ -1,11 +1,13 @@
 package com.example.demo.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.login.domain.model.Article;
 import com.example.demo.login.domain.model.ArticleForm;
@@ -48,5 +50,31 @@ public class ArticleController {
         }
 
         return "login/homeLayout";
+    }
+
+
+    @PostMapping(value="/articleEdit", params="update")
+    public String postArticleEditUpate(ArticleForm form, Model model) {
+        System.out.println("記事更新処理");
+
+        Article article = new Article();
+
+        article.setId(form.getId());
+        article.setTitle(form.getTitle());
+        article.setContent(form.getContent());
+
+        try {
+            boolean result = articleService.updateOne(article);
+
+            if (result == true) {
+                model.addAttribute("result", "更新成功");
+            } else {
+                model.addAttribute("result", "更新失敗");
+            }
+        } catch(DataAccessException e) {
+            model.addAttribute("result", "更新失敗");
+        }
+
+        return getArticleDetail(model, form.getId());
     }
 }
