19dd1b66b0d34e76fc0709a06aaa2ff1e76bdbaf
HironoriSato <maza.hs5360@gmail.com>
2019-04-28 00:46:49 +0900

    [update]編集画面表示機能をコントローラクラスに追加

--- src/main/java/com/example/demo/login/controller/ArticleController.java
+++ src/main/java/com/example/demo/login/controller/ArticleController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.login.domain.model.Article;
+import com.example.demo.login.domain.model.ArticleForm;
 import com.example.demo.login.domain.service.ArticleService;
 
 @Controller
@@ -28,5 +30,23 @@ public class ArticleController {
       }
 
       return "login/homeLayout";
+    }
+
+    @GetMapping("/articleEdit/{id}")
+    public String getArticleEdit(@ModelAttribute ArticleForm form, Model model, @PathVariable("id") Long articleId) {
+        System.out.println(articleId);
+
+        model.addAttribute("contents", "login/articleEdit :: articleEdit_contents");
+
+        if (articleId != null) {
+            Article article = articleService.selectOne(articleId);
+
+            form.setId(articleId);
+            form.setUserId(article.getUserId());
+            form.setTitle(article.getTitle());
+            form.setContent(article.getContent());
+        }
+
+        return "login/homeLayout";
     }
 }
